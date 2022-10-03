9827a69f021f9b7a028a7dbe3ca7ca1e3f7af723
root <averkievnv@gmail.com>
2022-10-03 23:04:58 +0300

    realized dependency injection MusicPlayer

--- src/main/java/ru/example/company/TestSpring.java
+++ src/main/java/ru/example/company/TestSpring.java
@@ -8,18 +8,8 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("classicalMusicBean", Music.class);
-        Music music1 = context.getBean("rockMusicBean", Music.class);
-        Music music2 = context.getBean("tranceMusicBean", Music.class);
-
-        MusicPlayer player = new MusicPlayer(music);
-        player.playMusic();
-
-        player = new MusicPlayer(music1);
-        player.playMusic();
-
-        player = new MusicPlayer(music2);
-        player.playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
         context.close();
     }
