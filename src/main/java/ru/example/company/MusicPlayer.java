7e3a3ee993a1dde59fc908e5973fe141f92422c7
root <averkievnv@gmail.com>
2022-10-03 23:45:09 +0300

    realized dependency injection MusicPlayer with setter

--- src/main/java/ru/example/company/MusicPlayer.java
+++ src/main/java/ru/example/company/MusicPlayer.java
@@ -1,13 +1,45 @@
 package ru.example.company;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+//    private Music music;
+    List<Music> musicList = new ArrayList<Music>();
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private int volume;
+
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+
+    public MusicPlayer() {
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
