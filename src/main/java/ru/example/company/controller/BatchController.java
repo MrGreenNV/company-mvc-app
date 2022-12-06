f5d26473dcd7f73edae68582ec2a11253759df0a
MrGreenNV <averkievnv@gmail.com>
2022-12-06 21:33:16 +0300

    add BatchController

---
+++ src/main/java/ru/example/company/controller/BatchController.java
@@ -0,0 +1,38 @@
+package ru.example.company.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.example.company.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/test-batch-update")
+public class BatchController {
+
+    public final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @GetMapping()
+    public String index() {
+        return "batch/index";
+    }
+
+    @GetMapping("without")
+    public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/with")
+    public String withBatch() {
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
+    }
+
+}
